5eceea1750c9568f9b15015ff9cd16aed17ff38f
OFRBG <ec2-user@ip-172-31-22-94.eu-central-1.compute.internal>
2017-05-22 00:36:20 +0000

    Changed command structure and added a meme.

--- bot.js
+++ bot.js
@@ -1,20 +1,25 @@
 //const pairs = ['AMP', 'ARDR', 'BCN', 'BCY', 'BELA', 'BLK', 'BTCD', 'BTM', 'BTS', 'BURST', 'CLAM', 'DASH', 'DCR', 'DGB', 'DOGE', 'EMC2', 'ETC', 'ETH', 'EXP', 'FCT', 'FLDC', 'FLO', 'GAME', 'GNO', 'GNT', 'GRC', 'HUC', 'LBC', 'LSK', 'LTC', 'MAID', 'NAUT', 'NAV', 'NEOS', 'NMC', 'NOTE', 'NXC', 'NXT', 'OMNI', 'PASC', 'PINK', 'POT', 'PPC', 'RADS', 'REP', 'RIC', 'SBD', 'SC', 'SJCX', 'STEEM', 'STR', 'STRAT', 'SYS', 'VIA', 'VRC', 'VTC', 'XBC', 'XCP', 'XEM', 'XMR', 'XPM', 'XRP', 'XVC', 'ZEC'];
 
+//var bittrex = require('bittrex-api');
 var fs = require('fs');	
 var keys = JSON.parse(fs.readFileSync('keys.api','utf8'))
 var prefix = '.tb'
 
-const pairs = ['ETH', 'ETC', 'GNT', 'XRP', 'LTC', 'BTC', 'XBT'];
+const pairs = ['ETH', 'ETC', 'GNT', 'XRP', 'LTC', 'BTC', 'XBT', 'MLN', 'ICN'];
 //const helpStr = '__**TsukiBot**__ :moon: \nCheck Kraken spot price in X: !\'tb gdax {ETH,LTC,BTC,XRP}\nCheck GDAX spot price in USD: !\'tb gdax {ETH,LTC,BTC}\nCheck large trades: !\'tb wh TICKER \nCheck volume records: !\'tb vol TICKER\n\n__Available tickers__\n'+pairs;
 
 
-const helpStr = '__**TsukiBot**__ :full_moon: \n```Markdown\n* ' + prefix + ' (k)rkn XXX [YYY] [op. base price]\n* ' + prefix + ' (g)dax XXX [op. base price]\n* ' + prefix + ' (w)h XXX (poloniex)\n* ' + prefix + ' (v)ol XXX (poloniex)```\n__Available tickers__\n`'+pairs + '`';
+const helpStr = '__**TsukiBot**__ :full_moon: \n```Markdown\n* ' + prefix + ' (k)rkn XXX [YYY] [op. base price]\n* ' + prefix + ' (g)dax XXX [op. base price]\n* ' + prefix + ' (w)h XXX (poloniex)\n* ' + prefix + ' (v)ol XXX (poloniex)\n* ' + prefix +' (e)scan address\n' + prefix +'g = ' + prefix + ' gdax eth\n' + prefix +'k = ' + prefix + ' krkn eth usd```\n__Available tickers__\n`'+pairs + '`\n`ETH tips to: 0x6A0D0eBf1e532840baf224E1bD6A1d4489D5D78d`';
 
 // Include discord.js
 const Discord = require('discord.js');
 
 const Client = require('coinbase').Client;
 const KrakenClient = require('kraken-api');
+
+
+var api = require('etherscan-api').init('7VWN4TVXM92WN56TW57ZD474R5S88JA4HF');
+
 
 
 // Declare channel and the channel to broadcast
@@ -81,10 +86,18 @@ function executeCommand(c, coin) {
 }
 
 
+
+function getEtherBalance(address){
+	var balance = api.account.balance(address);
+	balance.then(function(res){
+		channel.send('The total ether registered for `' + address + '` is: `' + res['result'] / 1000000000000000000 + ' ETH`.');
+	});
+}
+
+
 // Create a client and a token
 const client = new Discord.Client();
 const token = keys['discord'];
-
 
 client.on('ready', () => {
 	console.log('ready');	
@@ -94,9 +107,10 @@ client.on('ready', () => {
 	createLogger('gnt');	
 	createLogger('xrp');
 	createLogger('ltc');
-	
+
 	// Get the channel
 	channel = client.channels.find("name", channelName);
+	
 });
 
 client.on('message', message => {
@@ -115,30 +129,49 @@ client.on('message', message => {
 
 		// Remove the command prefix
 		code_in.splice(0,1);	
-
-		// Check if the command exists and it uses a valid pair
-		if(code_in.length > 1 && pairs.filter(function(value){return value == code_in[1].toUpperCase();}).length > 0){ 
-		
-			// vol (s) command: get the volume change for a period	
-			if(code_in[0] === 'vol' || code_in[0] === 'v'){
-				executeCommand('s', code_in[1])
-			} else if(code_in[0] === 'wh' || code_in[0] === 'w'){
-				executeCommand('p', code_in[1])
-			} else if(code_in[0] === 'gdax' || code_in[0] === 'g') {
-				getPriceGDAX(code_in[1], 'USD', (code_in[2] != null && !isNaN(code_in[2]) ? code_in[2] : -1))
-			} else if(code_in[0] === 'krkn' || code_in[0] === 'k') {
-				getPriceKraken(code_in[1], (code_in[2] == null ? 'USD' : code_in[2]), (code_in[3] != null && !isNaN(code_in[3]) ? code_in[3] : -1))
-			} else if(code_in[0] === 'trk'){
-				console.log('trk called.');
-			} else {
+		if(code_in.length > 1) {
+			// Check if the command exists and it uses a valid pair
+			if(pairs.filter(function(value){return value == code_in[1].toUpperCase();}).length > 0){ 		
+				// vol (s) command: get the volume change for a period	
+				if(code_in[0] === 'vol' || code_in[0] === 'v'){
+					executeCommand('s', code_in[1])
+				} else if(code_in[0] === 'wh' || code_in[0] === 'w'){
+					executeCommand('p', code_in[1])
+				} else if(code_in[0] === 'gdax' || code_in[0] === 'g') {
+					getPriceGDAX(code_in[1], 'USD', (code_in[2] != null && !isNaN(code_in[2]) ? code_in[2] : -1))
+				} else if(code_in[0] === 'krkn' || code_in[0] === 'k') {
+					getPriceKraken(code_in[1], (code_in[2] == null ? 'USD' : code_in[2]), (code_in[3] != null && !isNaN(code_in[3]) ? code_in[3] : -1))
+				} else if(code_in[0] === 'trk'){
+					console.log('trk called.');
+				} else {
+					const channel = client.channels.find("name", channelName);
+					channel.send(helpStr);
+				}
+			} else if(code_in[1].length == 42){
+				if(code_in[0] === 'escan' || code_in[0] === 'e') {
+					getEtherBalance(code_in[1]);
+				}
+			} else {	
 				const channel = client.channels.find("name", channelName);
 				channel.send(helpStr);
 			}
-		} else {	
-			const channel = client.channels.find("name", channelName);
-			channel.send(helpStr);
 		}
 			
+	} else if (code_in[0] === '.tbg') {
+		getPriceGDAX('ETH', 'USD', -1);
+	} else if (code_in[0] === '.tbk') {
+		getPriceKraken('ETH','USD',-1)
+	} else if (code_in[0] === '.help' || code_in[0] === '.th') {
+		const channel = client.channels.find("name", channelName);
+		channel.send(helpStr);
+	} else if (code_in[0] === '.dank') {
+		const channel = client.channels.find("name", channelName);
+		channel.send(":ok_hand:           :tiger:"+ '\n' + 
+		" :eggplant: :zzz: :necktie: :eggplant:"+'\n' + 
+		"                  :oil:     :nose:"+'\n' + 
+		"            :zap:  8=:punch: =D:sweat_drops:"+'\n' + 
+		"         :trumpet:   :eggplant:                       :sweat_drops:"+'\n' + 
+		"          :boot:    :boot:");	
 	}
 });
 
