afd842f8bcc8dc21a7eeb5159e55b6fb81895656
Logan <ultimate360solutions@gmail.com>
2019-03-23 21:41:39 -0500

    fixed storing and writing error

--- getCoinMeta.js
+++ getCoinMeta.js
@@ -1,60 +1,58 @@
-const fs                = require('fs');
-let pairs		= JSON.parse(fs.readFileSync("./common/coins.json","utf8"));
-const chalk             = require('chalk');
-let cmcMetaData         = {};
-const keys              = JSON.parse(fs.readFileSync('./common/keys.api','utf8'));
-const CoinMarketCap     = require('coinmarketcap-api');
-let clientcmc           = new CoinMarketCap(keys['coinmarketcap' + 2]);
-    let meta            = []; // empty Object
+const fs = require('fs');
+let pairs = JSON.parse(fs.readFileSync("./common/coins.json", "utf8"));
+const chalk = require('chalk');
+let cmcMetaData = {};
+const keys = JSON.parse(fs.readFileSync('./common/keys.api', 'utf8'));
+const CoinMarketCap = require('coinmarketcap-api');
+let clientcmc = new CoinMarketCap(keys['coinmarketcap' + 'failover']);
+let meta = {}; // empty Object
 
-                                                                            
 /* ---------------------------------
-
-  getCMCMetaData()
-
-  Updates once upon bot startup
-  and stores all coins meta data
-
+ 
+ getCMCMetaData()
+ 
+ Collects select metadata for coins
+ from CMC and saves it to file for
+ the bot to make use of. This function 
+ is meant to be run outside of the bot
+ as it will take a while to finish.
+ 
  ---------------------------------- */
-                                                                            
-function getCMCMetaData(){
+
+function getCMCMetaData() {
     let numberOfCoins = pairs.length;
-    console.log(numberOfCoins);
-    
-    for (let i=1; i !== numberOfCoins; i++) {
-    setTimeout( function timer(){
-      
-        let key = pairs[i];
-        
-        
-      clientcmc.getMetadata({symbol: key}).then(function(metaJSON){
-          //console.log(metaJSON);
-          let data = {
-            coin : key,
-            logo: metaJSON.data[key].logo,
-            description : metaJSON.data[key].description
-          };
-          meta.push(data);
-          console.log(chalk.blue("COMPLETED " + i + " OF " + numberOfCoins));
-        }).catch(console.error);
-        
-        if(i === numberOfCoins-1){
-           soupTime();
-        } 
-    }, i*2800 );
+    meta['contents'] = 'metadata';
+    meta['data'] = [];
+
+    for (let i = 1; i !== numberOfCoins + 1; i++) {
+        setTimeout(function timer() {
+            let key = pairs[i-1];
+            clientcmc.getMetadata({symbol: key}).then(function (metaJSON) {
+                //console.log(metaJSON);
+                let data = {
+                    coin: key,
+                    logo: metaJSON.data[key].logo,
+                    description: metaJSON.data[key].description
+                };
+                meta['data'].push(data);
+                console.log(chalk.blue("COMPLETED " + i + " OF " + numberOfCoins));
+            }).catch(console.error);
+
+            if (i === numberOfCoins) {
+                soupTime();
+            }
+        }, i * 2400);
     }
-    
 }
 
-function soupTime(){
+function soupTime() {
     console.log(meta);
-    
-    fs.writeFile("./common/metadata.json", meta, function (err) {
+    fs.writeFile("./common/metadata.json", JSON.stringify(meta), function (err) {
         if (err)
             return console.log(err);
     });
-    
     console.log(chalk.cyan("Operation completed successfully!"));
 }
 
+// run the function
 getCMCMetaData();
