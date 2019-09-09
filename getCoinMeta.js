d30a575842d80372c047fcd50d09f2e6153b8084
Logan <ultimate360solutions@gmail.com>
2019-09-09 04:16:36 -0500

    bugfix for metadata collection
    -fixed issue involving coins with same tickers where only the first coin's logo would be cached. Now each unique logo and set of metadata is cached correctly.

--- getCoinMeta.js
+++ getCoinMeta.js
@@ -1,11 +1,44 @@
 const fs = require('fs');
-let pairs = JSON.parse(fs.readFileSync("./common/coins.json", "utf8"));
 const chalk = require('chalk');
 let cmcMetaData = {};
 const keys = JSON.parse(fs.readFileSync('./common/keys.api', 'utf8'));
 const CoinMarketCap = require('coinmarketcap-api');
 let clientcmc = new CoinMarketCap(keys['coinmarketcap' + 'failover2']);
 let meta = {}; // empty Object
+let cmcArray = '';
+let cmcArrayDictParsed = '';
+let cmcArrayDict = '';
+
+
+
+/* ---------------------------------
+
+  getCMCData()
+
+  Create a dictionary of all coins
+  names and tickers to be used to 
+  reference their metadata.
+
+ ---------------------------------- */
+
+ async function getCMCData(){
+    //WARNING! This will pull ALL cmc coins and cost you about 11 credits on your api account!
+    let cmcJSON = await clientcmc.getTickers({limit: 2800}).then().catch(console.error);
+    cmcArray = cmcJSON['data'];
+    cmcArrayDictParsed = cmcArray;
+    cmcArrayDict = {};
+    try {
+        cmcArray.forEach(function(v){
+      if(!cmcArrayDict[v.symbol])
+        cmcArrayDict[v.symbol] = v;
+    });
+    } catch (err) { 
+      console.error(chalk.red.bold("failed to get cmc dictionary for metadata caching!" ));
+    }
+    //console.log(chalk.green(chalk.cyan(cmcArray.length) + " CMC tickers updated!"));
+  }
+
+
 
 /* ---------------------------------
  
@@ -20,19 +53,22 @@ let meta = {}; // empty Object
  ---------------------------------- */
 
 function getCMCMetaData() {
-    let numberOfCoins = pairs.length;
+    let numberOfCoins = cmcArrayDictParsed.length;
     meta['contents'] = 'metadata';
     meta['data'] = [];
 
     for (let i = 1; i !== numberOfCoins + 1; i++) {
         setTimeout(function timer() {
-            let key = pairs[i-1];
-            clientcmc.getMetadata({symbol: key}).then(function (metaJSON) {
+            let key = cmcArrayDictParsed[i-1];
+            //console.log(key);
+            clientcmc.getMetadata({id: key.id}).then(function (metaJSON) {
                 //console.log(metaJSON);
                 let data = {
-                    coin: key,
-                    logo: metaJSON.data[key].logo,
-                    description: metaJSON.data[key].description
+                    coin: key.symbol,
+                    name: key.name,
+                    slug: key.slug,
+                    logo: metaJSON.data[key.id].logo,
+                    description: metaJSON.data[key.id].description
                 };
                 meta['data'].push(data);
                 console.log(chalk.blue("COMPLETED " + i + " OF " + numberOfCoins));
@@ -54,5 +90,9 @@ function soupTime() {
     console.log(chalk.cyan("Operation completed successfully!"));
 }
 
-// run the function
-getCMCMetaData();
+// run the functions
+getCMCData();
+console.log(chalk.blue("Starting caching process, please wait.."));
+setTimeout(function(){
+    getCMCMetaData();
+}, 5000);
