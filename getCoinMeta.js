f14273a558b9a30cc89cd192933eeac852dbb00d
Logan <ultimate360solutions@gmail.com>
2021-01-26 01:40:59 -0600

    overhauled to now use coingecko for source

--- getCoinMeta.js
+++ getCoinMeta.js
@@ -1,100 +1,88 @@
 const fs = require('fs');
 const chalk = require('chalk');
-let cmcMetaData = {};
-const keys = JSON.parse(fs.readFileSync('./common/keys.api', 'utf8'));
-const CoinMarketCap = require('coinmarketcap-api');
-let clientcmc = new CoinMarketCap(keys['coinmarketcap' + 'failover2']);
-let meta = {}; // empty Object
-let cmcArray = '';
-let cmcArrayDictParsed = '';
-let cmcArrayDict = '';
+const CoinGecko = require('coingecko-api');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+var S = require('string');
+const CoinGeckoClient = new CoinGecko();
+let meta = { "data": [] };
+let count = 0;
+let cgCoinsList = "";
+
+
+async function collectMetadata(coin) {
+  let resJSON = await CoinGeckoClient.coins.fetch(coin, {
+    'localization': false, 'tickers': false,
+    'market_data': false, 'developer_data': false
+  });
+
+  //
+  // Starting with the hardest part, rebuilding the description string
+  //
+  let stringResponse = resJSON.data.description;
+  stringResponse = resJSON.data.description.en;
+  descDOM = new JSDOM(stringResponse);
+  let convertedLinks = [];
+
+  // Extract all of the html links, convert them to discord embed links, and then put them into an array
+  let elements = descDOM.window.document.getElementsByTagName('a');
+  for (let i = 0; i < elements.length; i++) {
+    let element = elements[i];
+    let url = element.href;
+    let hyperlinkText = element.text;
+    let discordHyperlink = `[${hyperlinkText}](${url})`;
+    convertedLinks.push(discordHyperlink);
+  }
+
+  // Replace each html link in the description string its the corresponding converted link we created earlier
+  for (let i = 0; i < convertedLinks.length; i++) {
+    let locatedString = S(stringResponse).between("<a href=\"", "</a>").s;
+    let lookupString = `<a href=\"${locatedString}</a>`;
+    stringResponse = stringResponse.replace(lookupString, convertedLinks[i]);
+  }
+
+  // Clean up the formatting
+  stringResponse = S(stringResponse).replaceAll('\r\n\r\n', '\n').s;
 
 
 
-/* ---------------------------------
-
-  getCMCData()
-
-  Create a dictionary of all coins
-  names and tickers to be used to 
-  reference their metadata.
-
- ---------------------------------- */
-
-async function getCMCData() {
-    //WARNING! This will pull ALL cmc coins and cost you about 11 credits on your api account!
-    let cmcJSON = await clientcmc.getTickers({ limit: 4200 }).then().catch(console.error);
-    cmcArray = cmcJSON['data'];
-    cmcArrayDictParsed = cmcArray;
-    cmcArrayDict = {};
-    try {
-        cmcArray.forEach(function (v) {
-            if (!cmcArrayDict[v.symbol])
-                cmcArrayDict[v.symbol] = v;
-        });
-    } catch (err) {
-        console.error(chalk.red.bold("failed to get cmc dictionary for metadata caching!"));
-    }
-    //console.log(chalk.green(chalk.cyan(cmcArray.length) + " CMC tickers updated!"));
+  // Now we can build this coins entry with its data and description, then add it to our main meta json
+  let coinMeta = {
+    id: ++count,
+    coin: resJSON.data.symbol,
+    name: resJSON.data.name,
+    slug: resJSON.data.id,
+    logo: resJSON.data.image.large,
+    description: stringResponse,
+    links: resJSON.data.links
+  };
+  meta.data.push(coinMeta);
 }
 
-
-
-/* ---------------------------------
- 
- getCMCMetaData()
- 
- Collects select metadata for coins
- from CMC and saves it to file for
- the bot to make use of. This function 
- is meant to be run outside of the bot
- as it will take a while to finish.
- 
- ---------------------------------- */
-
-function getCMCMetaData() {
-    let numberOfCoins = cmcArrayDictParsed.length;
-    meta['contents'] = 'metadata';
-    meta['data'] = [];
-
-    for (let i = 1; i !== numberOfCoins + 1; i++) {
-        setTimeout(function timer() {
-            let key = cmcArrayDictParsed[i - 1];
-            //console.log(key);
-            clientcmc.getMetadata({ id: key.id }).then(function (metaJSON) {
-                //console.log(metaJSON);
-                let data = {
-                    id: key.id,
-                    coin: key.symbol,
-                    name: key.name,
-                    slug: key.slug,
-                    logo: metaJSON.data[key.id].logo,
-                    description: metaJSON.data[key.id].description,
-                    links: metaJSON.data[key.id]["urls"]
-                };
-                meta['data'].push(data);
-                console.log(chalk.blue("COMPLETED " + chalk.cyan(i) + " OF " + chalk.cyan(numberOfCoins)));
-            }).catch(console.error);
-
-            if (i === numberOfCoins) {
-                soupTime();
-            }
-        }, i * 2400);
-    }
+function writeToFile() {
+  fs.writeFile("./common/metadata.json", JSON.stringify(meta), function (err) {
+    if (err)
+      return console.log(err);
+  });
+  console.log(chalk.greenBright("Caching operation completed successfully and file was written!"));
 }
 
-function soupTime() {
-    //console.log(meta);
-    fs.writeFile("./common/metadata.json", JSON.stringify(meta), function (err) {
-        if (err)
-            return console.log(err);
-    });
-    console.log(chalk.greenBright("Operation completed successfully!"));
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
 
-// run the functions
-getCMCData();
-console.log(chalk.blue("Starting caching process, please wait.."));
-setTimeout(function () {
-    getCMCMetaData();
-}, 5000);
+async function startup() {
+  cgCoinsList = await CoinGeckoClient.coins.list();
+  for (let i = 0; i < cgCoinsList.data.length; i++) {
+    console.log(chalk.cyan(cgCoinsList.data[i].id) + chalk.green(` (${i + 1} of ${cgCoinsList.data.length})`));
+    await collectMetadata(cgCoinsList.data[i].id);
+    await sleep(1000); //rate limiting requests to not exceed api limits
+  }
+  writeToFile();
+}
+
+startup();
+
+exports.run = startup;
