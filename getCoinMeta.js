4afe4faa7f2ac452c120a4eb22edb98c94b178ba
Logan <ultimate360solutions@gmail.com>
2023-05-04 11:21:28 -0500

    further improved reliability of metadata caching

--- getCoinMeta.js
+++ getCoinMeta.js
@@ -10,11 +10,13 @@ const CoinGecko = require('coingecko-api');
 const { JSDOM } = require('jsdom');
 const S = require('string');
 const CoinGeckoClient = new CoinGecko();
+const process = require('node:process');
 
 let meta = { 'data': [] };
 let skipped = [];
 let count = 0;
 let cgCoinsList = '';
+let queryTimeout = 15000; // milliseconds for sleeping between api calls to avoid rate limiting
 
 
 //* makes the call to the CoinGecko API and sets the resJSON global variable to the response
@@ -31,9 +33,10 @@ async function getCGdata(coin, index) {
     } catch {
       console.log(chalk.yellowBright(`Attempt ${chalk.magentaBright(attempt)} failed for ${chalk.cyanBright(coin)} : (${index})` + (attempt === 10 ? chalk.redBright(' ---> All attempts failed!') : ' --> Re-attempting')));
       attempt++;
-      await sleep(1000);
+      await sleep(queryTimeout);
     }
   }
+  if (attempt > 1) chalk.greenBright(`Attempt ${attempt} succeeded for ${chalk.cyanBright(coin)}`);
   return resJSON;
 }
 
@@ -46,11 +49,11 @@ async function collectMetadata(coin, index) {
     return;
   }
 
-  // Set the description re-formatted, otherwise just leave it blank if there isn't one found (the bot will handle this properly)
+  // set the description re-formatted, otherwise just leave it blank if there isn't one found (the bot will handle this properly)
   const desc = resJSON.data.description ? formatDescription(resJSON.data.description.en) : '';
-  if (!desc) console.log(chalk.magenta(`Blank description saved for: ${chalk.cyan(coin)} due to missing data. Proceeding...`));
+  if (!desc) console.log(chalk.magenta(`No description found for: ${chalk.cyan(coin)} - Saving a blank description and proceeding...`));
 
-  // Now can assemble a new meta object for this coin and then add it to the global meta json array for writing to file later
+  // now can assemble a new meta object for this coin and then add it to the global meta json array for writing to file later
   meta.data.push({
     id: ++count,
     coin: resJSON.data.symbol.toUpperCase(),
@@ -62,21 +65,28 @@ async function collectMetadata(coin, index) {
   });
 }
 
-// Formatting and cleaning up data in the description field for the coin
+//* formatting and cleaning up data in the description field for the coin
 function formatDescription(description) {
   const descDOM = new JSDOM(description);
   const elements = descDOM.window.document.getElementsByTagName('a');
   const convertedLinks = Array.from(elements).map(e => `[${e.text}](${e.href})`);
 
-  // Replace each html link in the description string its the corresponding converted link we created earlier
+  // replace each html link in the description string its the corresponding converted link we created earlier
   convertedLinks.forEach(link => {
     const locatedString = S(description).between('<a href="', '</a>').s;
     const lookupString = `<a href="${locatedString}</a>`;
     description = description.replace(lookupString, link);
   });
 
+  // clean up the newline formatting and whitespace, then return the description
   // Clean up the newline formatting and whitespace, then return the description
-  return S(description).collapseWhitespace().replaceAll('\r\n', '\n').s;
+  try {
+    return S(description).collapseWhitespace().replaceAll('\r\n', '\n').s;
+  }
+  catch (e) {
+    console.error(chalk.yellow('Description formatting failed, returning a blank string. Error details:\n', e));
+    return '';
+  }
 }
 
 //* once all of the coins have been collected and had their data formatted, this will get called to write the meta object to a file
@@ -96,17 +106,20 @@ function sleep(ms) {
 //* starts the process of collecting the metadata for all of the coins and handles rate limiting and calling of the other functions
 async function startup() {
   cgCoinsList = await CoinGeckoClient.coins.list();
+  if (cgCoinsList == 0) {
+    console.log(chalk.red('Could not grab coins list, likely currently rate limited or API is down. Exiting..'));
+    process.exit(1);
+  }
   for (let i = 0; i < cgCoinsList.data.length; i++) {
     const coinData = cgCoinsList.data[i];
     const progress = chalk.green(` (${i + 1} of ${cgCoinsList.data.length})`);
-
     if (!coinData.id) {
       console.log(chalk.yellow('NO ID FOUND [SKIPPED]') + progress);
     } else {
       console.log(chalk.cyan(coinData.id) + progress);
       await collectMetadata(coinData.id, i + 1);
     }
-    await sleep(15000); // Rate limiting requests to not exceed API limits
+    await sleep(queryTimeout); // Rate limiting requests to not exceed API limits
   }
   writeToFile();
 }
