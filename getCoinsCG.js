83bf0c8075d5e55d9e6a37e51807452860038933
Logan <ultimate360solutions@gmail.com>
2026-01-12 10:38:45 -0600

    Refactor cg api call to use raw fetch() instead of library
    Replaced CoinGecko API client usage with direct fetch call to CoinGecko API. Updated JSON file writing to reflect new data structure.

--- getCoinsCG.js
+++ getCoinsCG.js
@@ -9,31 +9,29 @@ const chalk = require('chalk');
 //
 //
 
-
-// Import coingecko-api
-const CoinGecko = require('coingecko-api');
-
-// Initiate the CoinGecko API Client
-const CoinGeckoClient = new CoinGecko();
-
 // Make API call and do JSON build operation
 var update = async () => {
   let data, tickers = [];
   try {
-    data = await CoinGeckoClient.coins.list();
-    for (const value of data.data) {
+    const response = await fetch('https://api.coingecko.com/api/v3/coins/list?include_platform=false');
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    
+    data = await response.json();
+    
+    for (const value of data) {
       tickers.push(value.symbol.toUpperCase());
     }
   } catch (err) {
-    if (data?.code) {
-      console.log(chalk.red(`Unable to grab list of all CG coins: Error ${data.code}: ${data.message}`));
-    }
+    console.log(chalk.red(`Unable to grab list of all CG coins: ${err.message}`));
   }
 
   //console.log(data);
 
   // Write the identification JSON to file
-  fs.writeFileSync('./common/coinsCG.json', JSON.stringify(data.data));
+  fs.writeFileSync('./common/coinsCG.json', JSON.stringify(data));
   fs.writeFileSync('./common/coinsCGtickers.json', JSON.stringify(tickers));
   //console.log("CoinGecko coin list complete!");
 };
